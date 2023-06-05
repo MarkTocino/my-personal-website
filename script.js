a293766f496354f2bd0c448f0973219a2fc07254
MarkTocino <Mark_tocino@yahoo.com>
2023-06-05 02:54:44 -0400

    Added 2 alerts picture and a white background.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by Javascript";
 document.querySelector("body").appendChild(h2);
+function clickMe() {
+    alert("This is absolutely exciting to do! And I am still learning. Can't wait!, I will make a second Button Which counts numbers up to 10!")
+};
+function countNumbers() {
+    for (let i = 0; i <= 10; i++) {
+      alert(i);
+    }
+};
